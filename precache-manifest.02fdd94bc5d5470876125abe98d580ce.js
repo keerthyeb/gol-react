e02b3e1e885ce297eb1780508ba3512be0ea0205
Keerthyb <keerthyb@IN-Keerthyb.local>
2019-04-02 15:55:04 +0530

    Updates

---
+++ precache-manifest.02fdd94bc5d5470876125abe98d580ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a418cfaab7d7e520cf4d",
+    "url": "%20//keerthyeb.github.io/gol-react/static/css/main.b4d697f0.chunk.css"
+  },
+  {
+    "revision": "a418cfaab7d7e520cf4d",
+    "url": "%20//keerthyeb.github.io/gol-react/static/js/main.d28ed0c6.chunk.js"
+  },
+  {
+    "revision": "f01e0a1c7e933260fd59",
+    "url": "%20//keerthyeb.github.io/gol-react/static/js/runtime~main.c92fe3ff.js"
+  },
+  {
+    "revision": "418ff13513de877b9200",
+    "url": "%20//keerthyeb.github.io/gol-react/static/js/2.56a8f672.chunk.js"
+  },
+  {
+    "revision": "ab913a00b3061c748e3b5048f525c4ee",
+    "url": "%20//keerthyeb.github.io/gol-react/index.html"
+  }
+];
